aeeb22dd227ee3f536ee981f4e443f866f8cf843
Managam Silalahi <managam.silalahi@gmail.com>
2016-12-26 20:15:36 +0700

    Pattern: Object Template Pattern

---
+++ Chapter 4/ObjectTemplate/ObjectTemplate/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ObjectTemplate
+//
+//  Created by Managam Silalahi on 12/26/16.
+//  Copyright © 2016 Managam. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
