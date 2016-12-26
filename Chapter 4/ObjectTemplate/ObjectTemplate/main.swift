e90e39811f1925478d55ed42ad5fc4611cd06f22
Managam Silalahi <managam.silalahi@gmail.com>
2016-12-26 20:27:46 +0700

    Understanding the Problem Addressed by the Pattern

--- Chapter 4/ObjectTemplate/ObjectTemplate/main.swift
+++ Chapter 4/ObjectTemplate/ObjectTemplate/main.swift
@@ -8,5 +8,21 @@
 
 import Foundation
 
-print("Hello, World!")
+var products = [("Kayak", "A boat for one person", 275.0, 10),
+    ("Lifejacket", "Protective and fashionable", 48.95, 14),
+    ("Soccer Ball", "FIFA-approved size and weight", 19.5, 32)]
+
+func calculateTax(product: (String, String, Double, Int) ) -> Double {
+    return product.2 * 0.2
+}
+
+func calculateStockValue(tuples: [(String, String, Double, Int)]) -> Double {
+    return tuples.reduce(0, { (total, product) -> Double in
+        return total + (product.2 * Double(product.3))
+    })
+}
+
+print("Sales tax for Kayak: \(calculateTax(product: products[0]))")
+print("Total value of Stock: \(calculateStockValue(tuples: products))")
+
 
