366249a08f8b224b0af3e5cb01826a830c1e7414
Managam Silalahi <managam.silalahi@gmail.com>
2016-12-27 20:05:54 +0700

    Implementing the Object Template Pattern

---
+++ Chapter 4/ObjectTemplate/ObjectTemplate/Product.swift
@@ -0,0 +1,23 @@
+//
+//  Product.swift
+//  ObjectTemplate
+//
+//  Created by Managam Silalahi on 12/27/16.
+//  Copyright © 2016 Managam. All rights reserved.
+//
+
+import Foundation
+
+class Product {
+    var name: String = ""
+    var description: String = ""
+    var price: Double = 0.0
+    var stock: Int = 0
+    
+    init(name: String, description: String, price: Double, stock: Int) {
+        self.name = name
+        self.description = description
+        self.price = price
+        self.stock = stock
+    }
+}
--- Chapter 4/ObjectTemplate/ObjectTemplate/main.swift
+++ Chapter 4/ObjectTemplate/ObjectTemplate/main.swift
@@ -8,21 +8,24 @@
 
 import Foundation
 
-var products = [("Kayak", "A boat for one person", 275.0, 10),
-    ("Lifejacket", "Protective and fashionable", 48.95, 14),
-    ("Soccer Ball", "FIFA-approved size and weight", 19.5, 32)]
+var products = [Product(name: "Kayak", description: "A boat for one person",
+                        price: 275, stock: 10),
+                Product(name: "Lifejacket", description: "Protective and fashionable",
+                        price: 48.95, stock: 14),
+                Product(name: "Soccer Ball", description: "FIFA-approved size and weight",
+                        price: 19.5, stock: 32)]
 
-func calculateTax(product: (String, String, Double, Int) ) -> Double {
-    return product.2 * 0.2
+func calculateTax(product: Product) -> Double {
+    return product.price * 0.2
 }
 
-func calculateStockValue(tuples: [(String, String, Double, Int)]) -> Double {
-    return tuples.reduce(0, { (total, product) -> Double in
-        return total + (product.2 * Double(product.3))
+func calculateStockValue(productsArray: [Product]) -> Double {
+    return productsArray.reduce(0, { (total, product) -> Double in
+        return total + (product.price + Double(product.stock))
     })
 }
 
-print("Sales tax for Kayak: \(calculateTax(product: products[0]))")
-print("Total value of Stock: \(calculateStockValue(tuples: products))")
+print("Sales tax for Kayak: $\(calculateTax(product: products[0]))");
+print("Total value of stock: $\(calculateStockValue(productsArray: products))");
 
 
