0b1551ef6d1c393475570ae1b8c30b9176207cff
Managam Silalahi <managam.silalahi@gmail.com>
2017-01-02 15:09:39 +0700

    Understanding Shallow and Deep Copying

--- Chapter 5/ReferenceTypes.playground/Contents.swift
+++ Chapter 5/ReferenceTypes.playground/Contents.swift
@@ -2,19 +2,29 @@
 
 import UIKit
 
-class Appointment: NSCopying {
+class Location {
+    var name: String
+    var address: String
+    
+    init(name: String, address: String) {
+        self.name = name
+        self.address = address
+    }
+}
+
+class Appointment {
     var name: String
     var day: String
-    var place: String
+    var place: Location
     
-    init(name: String, day: String, place: String) {
+    init(name: String, day: String, place: Location) {
         self.name = name
         self.day = day
         self.place = place
     }
     
     func printDetails(label: String) {
-        print("\(label) with \(name) on \(day) at \(place)")
+        print("\(label) with \(name) on \(day) at at \(place.name) \(place.address)")
     }
     
     func copy(with zone: NSZone? = nil) -> Any {
@@ -22,11 +32,12 @@ class Appointment: NSCopying {
     }
 }
 
-var beerMeeting = Appointment(name: "Bob", day: "Mon", place: "Joe's Bar")
+var beerMeeting = Appointment(name: "Bob", day: "Mon", place:  Location(name:"Joe's Bar", address: "123 Main St"))
 var workMeeting = beerMeeting.copy() as! Appointment
 workMeeting.name = "Alice"
 workMeeting.day = "Fri"
-workMeeting.place = "Conference Rm 2"
+workMeeting.place.name = "Conference Rm 2"
+workMeeting.place.address = "Company HQ"
 
 beerMeeting.printDetails(label: "Social")
 workMeeting.printDetails(label: "Work")
