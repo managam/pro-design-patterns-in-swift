00324db5e73600595f4800f8eeb02a3906a23d1a
Managam Silalahi <managam.silalahi@gmail.com>
2017-01-02 17:32:24 +0700

    (Not Really) Solving the Underlying Problem

--- Chapter 5/Hiding.playground/Contents.swift
+++ Chapter 5/Hiding.playground/Contents.swift
@@ -16,13 +16,26 @@ class MessageLogger {
     var messages: [Message] = []
     
     func logMessage(message: Message) {
-        messages.append(Message(to: message.to, subject: message.subject))
+        if let detailed = message as? DetailedMessage  {
+            messages.append(DetailedMessage(to: detailed.to, subject: detailed.subject, from: detailed.from))
+        } else {
+            messages.append(Message(to: message.to, subject: message.subject))
+        }
     }
     
     func processMessage(callback: (Message) -> Void) {
         for message in messages {
             callback(message)
         }
+    }
+}
+
+class DetailedMessage: Message {
+    var from: String
+    
+    init(to: String, subject: String, from: String ) {
+        self.from = from
+        super.init(to: to, subject: subject)
     }
 }
 
@@ -34,7 +47,14 @@ message.to = "Bob"
 message.subject = "Free for dinner?"
 logger.logMessage(message: message)
 
-logger.processMessage { (message) in
-    print("Message - to: \(message.to), Subject: \(message.subject)")
-}
+var detailedMessage = DetailedMessage(to: "Alice", subject: "Hi!", from: "Joe")
+logger.logMessage(message: detailedMessage)
+
+logger.processMessage(callback: { (message) in
+    if let detailed = message as? DetailedMessage  {
+        print("Detailed Message - To: \(detailed.to) From: \(detailed.from) Subject: \(detailed.subject)")
+    } else {
+        print("Message - To: \(message.to) Subject: \(message.subject)");
+    }
+})
 
