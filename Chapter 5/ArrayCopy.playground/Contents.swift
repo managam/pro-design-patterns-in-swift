bd6f72a44c64af0b1e98da7bdb11a67e40e3c4be
Managam Silalahi <managam.silalahi@gmail.com>
2017-01-02 15:38:02 +0700

    Copying Arrays Deep Copying

--- Chapter 5/ArrayCopy.playground/Contents.swift
+++ Chapter 5/ArrayCopy.playground/Contents.swift
@@ -16,9 +16,17 @@ class Person: NSCopying {
     }
 }
 
+func deepCopy(data: [Any]) -> [Any] {
+    return data.map({ (item) -> Any in
+        return (item is NSCopying) ? (item as! Person).copy() : item
+    })
+}
+
 var people = [Person(name:"Joe", country:"France"),
               Person(name:"Bob", country:"USA")]
-var otherPeople = people
+//var otherPeople = people
+var otherPeople = deepCopy(data: people) as! [Person]
 
 people[0].country = "UK"
 print("Country \(otherPeople[0].country)")
+print("Country \(people[0].country)")
