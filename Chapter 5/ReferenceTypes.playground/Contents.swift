c7e524387188ac32b79c3351aa62af58888448f3
Managam Silalahi <managam.silalahi@gmail.com>
2017-01-02 13:42:29 +0700

    Closing Reference Types

---
+++ Chapter 5/ReferenceTypes.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+class Appointment {
+    var name: String
+    var day: String
+    var place: String
+    
+    init(name: String, day: String, place: String) {
+        self.name = name
+        self.day = day
+        self.place = place
+    }
+    
+    func printDetails(label: String) {
+        print("\(label) with \(name) on \(day) at \(place)")
+    }
+}
+
+var beerMeeting = Appointment(name: "Bob", day: "Mon", place: "Joe's Bar")
+var workMeeting = beerMeeting
+workMeeting.name = "Alice"
+workMeeting.day = "Fri"
+workMeeting.place = "Conference Rm 2"
+
+beerMeeting.printDetails(label: "Social")
+workMeeting.printDetails(label: "Work")
