a992e68b46cc86ca334b730fbf58a8c0e314c2b8
Managam Silalahi <managam.silalahi@gmail.com>
2017-01-02 15:53:52 +0700

    Understanding the Benefits of the Prototype Pattern, Avoiding Expensive Initializations

--- Chapter 5/Initialization.playground/Contents.swift
+++ Chapter 5/Initialization.playground/Contents.swift
@@ -2,7 +2,7 @@
 
 import UIKit
 
-class Sum {
+class Sum: NSCopying {
     var resultsCache: [[Int]] = []
     var firstValue:Int = 0
     var secondValue:Int = 0
@@ -24,9 +24,25 @@ class Sum {
         self.firstValue = first;
         self.secondValue = second;
     }
+    
+    private init(first: Int, second: Int, cache: [[Int]]) {
+        self.firstValue = first
+        self.secondValue = second
+        resultsCache = cache
+    }
+    
+    func copy(with zone: NSZone? = nil) -> Any {
+        return Sum(first: self.firstValue, second: self.secondValue, cache: self.resultsCache)
+    }
 }
 
-var calc1 = Sum(first: 0, second: 9).result
-var calc2 = Sum(first: 3, second: 8).result
+//var calc1 = Sum(first: 0, second: 9).result
+//var calc2 = Sum(first: 3, second: 8).result
+
+var prototype = Sum(first:0, second:9)
+var calc1 = prototype.result
+var clone = prototype.copy() as! Sum
+clone.firstValue = 3; clone.secondValue = 8
+var calc2 = clone.result
 
 print("Calc1: \(calc1) Calc2: \(calc2)")
