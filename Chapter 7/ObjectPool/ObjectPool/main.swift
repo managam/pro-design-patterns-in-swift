88313e369746a751415809d8d7d7a17c574e424b
Managam Silalahi <managam.silalahi@gmail.com>
2017-02-22 20:04:53 +0700

    ObjectPool project init

---
+++ Chapter 7/ObjectPool/ObjectPool/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ObjectPool
+//
+//  Created by Managam Silalahi on 2/22/17.
+//  Copyright © 2017 Managam. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
