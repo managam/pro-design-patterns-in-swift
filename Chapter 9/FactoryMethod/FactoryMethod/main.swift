2162a504bce30fe1da1f652c994655af68ec04fb
Managam Silalahi <managam.silalahi@gmail.com>
2017-03-08 21:09:40 +0700

    Initialize FactoryMethod project

---
+++ Chapter 9/FactoryMethod/FactoryMethod/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FactoryMethod
+//
+//  Created by Managam Silalahi on 3/8/17.
+//  Copyright © 2017 Managam. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
