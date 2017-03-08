154648ea19af1d2a5c4467729b631fcb16e4ecec
Managam Silalahi <managam.silalahi@gmail.com>
2017-03-08 21:04:41 +0700

    // Listing 7-6. Testing the Library and Pool in the main.swift File

---
+++ Chapter 7/ObjectPool/ObjectPool/Library.swift
@@ -0,0 +1,53 @@
+//
+//  Library.swift
+//  ObjectPool
+//
+//  Created by Managam Silalahi on 2/23/17.
+//  Copyright © 2017 Managam. All rights reserved.
+//
+
+import Foundation
+
+final class Library {
+    private var books: [Book]
+    private let pool: Pool<Book>
+    
+    private init(stockLevel: Int) {
+        books = [Book]()
+        for count in 1 ... stockLevel {
+            books.append(Book(author: "Managam, Silalahi", title: "The heart of God", stock: count))
+        }
+        self.pool = Pool<Book>(items: books)
+    }
+    
+    private class var singleton: Library {
+        struct SingletonWrapper {
+            static let singleton = Library(stockLevel: 2)
+        }
+        return SingletonWrapper.singleton
+    }
+    
+    class func checkoutBook(reader: String) -> Book? {
+        let book = singleton.pool.getFromPool()
+        book?.reader = reader
+        book?.checkoutCount += 1
+        return book
+    }
+    
+    class func returnBook(book: Book) {
+        book.reader = nil
+        singleton.pool.returnToPool(item: book)
+    }
+    
+    class func printReport() {
+        for book in singleton.books {
+            print("...Book#\(book.stockNumber)...")
+            print("Checked out \(book.checkoutCount) times")
+            if book.reader != nil {
+                print("Checked out to \(book.reader)")
+            } else {
+                print("In stock")
+            }
+        }
+    }
+}
--- Chapter 7/ObjectPool/ObjectPool/main.swift
+++ Chapter 7/ObjectPool/ObjectPool/main.swift
@@ -8,5 +8,25 @@
 
 import Foundation
 
-print("Hello, World!")
+// Listing 7-6. Testing the Library and Pool in the main.swift File
+
+var queue = DispatchQueue(label: "workQ", attributes: .concurrent)
+var group = DispatchGroup()
+
+print("Starting...")
+
+for i in 1 ... 3 {
+    queue.async(group: group, execute: {
+        var book = Library.checkoutBook(reader: "reader#\(i)")
+        if book != nil {
+            sleep(arc4random() % 2)
+            Library.returnBook(book: book!)
+        }
+    })
+}
+
+group.wait(timeout: .distantFuture)
+
+print("All block are complete")
+Library.printReport()
 
