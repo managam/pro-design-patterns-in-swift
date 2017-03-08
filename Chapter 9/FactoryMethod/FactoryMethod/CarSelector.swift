3f2c2cee6f4ed265143f49f5046cc0016195b432
Managam Silalahi <managam.silalahi@gmail.com>
2017-03-08 21:28:03 +0700

    Defining a Global Factory Method

--- Chapter 9/FactoryMethod/FactoryMethod/CarSelector.swift
+++ Chapter 9/FactoryMethod/FactoryMethod/CarSelector.swift
@@ -8,24 +8,27 @@
 
 import Foundation
 
-class CarSelector {
+func createRentalCar(passengers: Int) -> RentalCar? {
+    var car: RentalCar?
     
+    switch (passengers) {
+    case 0...1:
+        car = Sports()
+    case 2...3:
+        car = Compact()
+    case 4...8:
+        car = SUV()
+    case 9...14:
+        car = Minivan()
+    default:
+        car = nil
+    }
+    
+    return car
+}
+
+class CarSelector {
     class func selectCar(passengers: Int) -> String? {
-        var car: RentalCar?
-        
-        switch passengers {
-        case 0...1:
-            car = Sports()
-        case 2...3:
-            car = Compact()
-        case 4...8:
-            car = SUV()
-        case 9...14:
-            car = Minivan()
-        default:
-            car = nil
-        }
-        
-        return car?.name
+        return createRentalCar(passengers: passengers)?.name
     }
 }
--- Chapter 9/FactoryMethod/FactoryMethod/PriceCalculator.swift
+++ Chapter 9/FactoryMethod/FactoryMethod/PriceCalculator.swift
@@ -9,23 +9,8 @@
 import Foundation
 
 class PriceCalculator {
-    
     class func calculatePrice(passengers: Int, days: Int) -> Float? {
-        var car: RentalCar?
-        
-        switch (passengers) {
-        case 0...1:
-            car = Sports()
-        case 2...3:
-            car = Compact()
-        case 4...8:
-            car = SUV()
-        case 9...14:
-            car = Minivan()
-        default:
-            car = nil
-        }
-        
+        let car = createRentalCar(passengers: passengers)
         return car == nil ? nil : car!.pricePerDay * Float(days)
     }
 }
